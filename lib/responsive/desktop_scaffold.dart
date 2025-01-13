9424a6ae72dd62a5647adbc8c599c871e393d1f6
tboy <gluestick829@gmail.com>
2025-01-13 18:44:54 +0900

    Add: desktop scaffoldにwidgetを追加

--- lib/responsive/desktop_scaffold.dart
+++ lib/responsive/desktop_scaffold.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mk_responsive_dashboard/constants.dart';
+import 'package:mk_responsive_dashboard/util/my_box.dart';
+import 'package:mk_responsive_dashboard/util/my_tile.dart';
 
 class DesktopScaffold extends StatefulWidget {
   const DesktopScaffold({super.key});
@@ -20,6 +22,51 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
           myDrawer,
 
           // rest of body
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                // 4 box on the top
+                AspectRatio(
+                  aspectRatio: 4,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GridView.builder(
+                      itemCount: 4,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 4),
+                      itemBuilder: (context, index) {
+                        return const MyBox();
+                      },
+                    ),
+                  ),
+                ),
+
+                // tiles below it
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return const MyTile();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.pink,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
